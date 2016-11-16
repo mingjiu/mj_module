b90e86e146b0be4f4d711d1e22744e1d11b1f6e3
mjhuang <hmj0718@163.com>
2016-11-16 16:25:39 +0800

    Revert "基本功能完成"
    This reverts commit fa97b861356f45731297b876e84c6da496b94881.

--- jsText.js
+++ jsText.js
@@ -1,20 +1,11 @@
 function jsText(){
     var fs = require("fs");
-    var all = fs.readFileSync(__dirname+'/public/dist/modules/index.min.js',"utf-8");
-    all = all.split('//module//');
-    var jsMap = {};
-    for(var i = 1; i < all.length; i++){
-        var temp = all[i].split('//\n');
-        if(i == 1){
-            temp[1] = '(' + temp[1].substring(1, temp[1].length-2) + ');';
-        }else if(i == all.length - 1){
-            temp[1] = '(' + temp[1].substring(0, temp[1].length-1) + ');';
-        }else{
-            temp[1] = '(' + temp[1].substring(0, temp[1].length-2) + ');';
-        }
-        jsMap[temp[0]] = temp[1];
+    var js = fs.readFileSync(__dirname+'/public/dist/all.min.js',"utf-8");
+
+    js = js.split('var mjModule="mjModule";');
+    for(var i in js){
+        console.log(js[i]);
     }
-    // console.log(jsMap);
-    return jsMap;
+
 }
 module.exports = jsText;
--- public/src/js/modules/dom.js
+++
@@ -1,7 +0,0 @@
-//module//dom//
-(function(window, Man){
-    var dom  = window.document.getElementById('testid');
-    dom.innerHTML = '这是个测试操作';
-    window.testMan = new Man('testMan');
-    console.log(window.testMan);
-})(window, Man);
